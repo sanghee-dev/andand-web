32589482b051a4f577e1a074809d86e9445c912b
leeedev <leeesangheee@gmail.com>
2021-04-18 22:19:16 +0900

    Router Setup

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,27 @@
-import React from "react";
+import * as React from "react";
+import {
+  HashRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
 
-const App = () => {
-  return <div>Andand</div>;
+const App: React.FC = () => {
+  const isLoggedIn = false;
+
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div>Home</div>
+        </Route>
+        <Route path="/potato">
+          <div>potato</div>
+        </Route>
+        <Redirect from="*" to="/" />
+      </Switch>
+    </Router>
+  );
 };
 
 export default App;
