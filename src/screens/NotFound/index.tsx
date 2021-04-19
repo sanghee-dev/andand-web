42188701c41297ceac1c555cdff0deb357be97b1
leeedev <leeesangheee@gmail.com>
2021-04-19 20:38:01 +0900

    Add styles to NotFound

--- src/screens/NotFound/index.tsx
+++ src/screens/NotFound/index.tsx
@@ -1,5 +1,21 @@
+import styled, { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "styles/styles";
+import { useReactiveVar } from "@apollo/client";
+import { darkModeVar } from "apollo";
+
+const Title = styled.h1`
+  background-color: ${(props) => props.theme.bgColor};
+  color: ${(props) => props.theme.fontColor};
+`;
+
 const NotFound = () => {
-  return <h1>404 not found</h1>;
+  const darkMode = useReactiveVar(darkModeVar);
+
+  return (
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+      <Title>404 not found</Title>
+    </ThemeProvider>
+  );
 };
 
 export default NotFound;
