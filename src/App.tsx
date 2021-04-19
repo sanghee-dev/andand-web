8c2868f77ed13b6a07c47fbe0eadb84c178668cb
leeedev <leeesangheee@gmail.com>
2021-04-19 20:38:10 +0900

    Code Cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,17 @@
-import * as React from "react";
-import { HashRouter as Router, Route, Switch } from "react-router-dom";
-import { useReactiveVar } from "@apollo/client";
-import styled, { ThemeProvider } from "styled-components";
+import { ThemeProvider } from "styled-components";
 import { GlobalStyles, lightTheme, darkTheme } from "styles/styles";
-import Home from "screens/Home";
-import Login from "screens/Login";
-import NotFound from "screens/NotFound";
+import { useReactiveVar } from "@apollo/client";
+import AppRouter from "AppRouter";
 import { isLoggedInVar, darkModeVar } from "apollo";
 
-interface IContainerProps {
-  floating: boolean;
-}
-
-const Container = styled.div<IContainerProps>`
-  background-color: ${(props) => props.theme.bgColor};
-  color: ${(props) => props.theme.fontColor};
-  border-bottom: ${(props) =>
-    props.floating ? "1px solid blue" : "1px solid red"};
-`;
-
-const App: React.FC = () => {
+const App = () => {
   const isLoggedIn = useReactiveVar(isLoggedInVar);
   const darkMode = useReactiveVar(darkModeVar);
 
   return (
     <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <GlobalStyles />
-      <Container floating={true}>
-        <h1>Andand</h1>
-      </Container>
-      <Router>
-        <Switch>
-          <Route path="/" exact>
-            {isLoggedIn ? <Home /> : <Login />}
-          </Route>
-          <Route path="/*" component={NotFound} />
-        </Switch>
-      </Router>
+      <AppRouter isLoggedIn={isLoggedIn} />
     </ThemeProvider>
   );
 };
---
+++ src/AppRouter.tsx
@@ -0,0 +1,23 @@
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "screens/Home";
+import Login from "screens/Login";
+import NotFound from "screens/NotFound";
+
+interface Props {
+  isLoggedIn: boolean;
+}
+
+const AppRouter = ({ isLoggedIn }: Props) => {
+  return (
+    <Router>
+      <Switch>
+        <Route path="/" exact>
+          {isLoggedIn ? <Home /> : <Login />}
+        </Route>
+        <Route path="/*" component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
